c9c4935896d0ff3ca0b82898836f89161b80e096
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-20 22:28:17 +0100

    added color to cube

--- openglPython.py
+++ openglPython.py
@@ -29,8 +29,40 @@ edges = (
     (5, 7)
 )
 
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
+
+colors = (
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (1, 1, 1),
+    (0, 0, 0),
+    (1, 0, 0),
+    (1, 1, 0),
+    (0, 1, 1),
+    (0, 0, 0),
+    (1, 0, 1),
+    (1, 0, 0),
+    (1, 0, 0),
+)
+
 
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x += 1
+            glColor3fv(colors[x])
+            glVertex3fv(vertices[vertex])
+    glEnd()
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -55,5 +87,6 @@ def main():
         Cube()
         pygame.display.flip()
         pygame.time.wait(10)
+
 
 main()
